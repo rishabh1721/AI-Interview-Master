42fd0b0d050025f55f28c60233de13cf9874408f
rishabh1721 <rishabhmcu001@gmail.com>
2026-05-31 00:37:21 +0530

    backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    origin: [
+      "http://localhost:5173",
+      "https://ai-interview-master-1.onrender.com",
+    ],
     credentials: true,
   }),
 );
